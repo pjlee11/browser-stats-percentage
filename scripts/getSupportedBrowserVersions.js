961acfc102058e6a0b5e5bcc81e612ba598d4f24
Phil Lee <phil.lee.ext@bbc.co.uk>
2018-12-13 15:08:54 +0000

    use newly made helper functions

--- scripts/getSupportedBrowserVersions.js
+++ scripts/getSupportedBrowserVersions.js
@@ -1,183 +1,45 @@
 /* 
   This node process takes the JSON and creates an object that is keyed per service. 
-  Then for each service each differing browser and version as a string is keyed. 
+  Then for each service it contains each differing browser and version as a keyed string. 
   
-  From this object each service is validated to be a correct bbc_site value.
+  From this object each service is validated to be a correct 'bbc_site' value.
   Then each browser & version entry is checked to ensure it's above the 0.05% support threshold. 
-  
-    // TODO: The above section should be generic for each node process and should be re-usable/extendable code. 
 
-  Following this it splits the browser name and version into seperate fields.
-  It then creates a new object with the browser keyed and the versions as entries. 
-  Finally it logs out the lowest supported version for each browser. 
-
-  TL;DR - it outputs the browsers we should aim support as they are within our threshold of support.
+  Following this it creates an array of versions for each browser.
+  Finally it logs out the lowest supported version for each browser per service. 
 */
 
 const fs = require("fs");
-const data = fs.readFileSync("./browser-stats.json");
-const { isValidBBCSiteValue } = require("../utility");
+const dataAsJSON = fs.readFileSync("./browser-stats.json", "utf8");
+const { removeInvalidValues } = require("./helpers/removeInvalidValues");
+const {
+  getServiceTotalBrowsers
+} = require("./helpers/getServiceTotalBrowsers");
+const { getDataWithinThreshold } = require("./helpers/getDataWithinThreshold");
+const {
+  seperateBrowserNameAndVersion
+} = require("./helpers/seperateBrowserNameAndVersion");
 
-let dataKeyedPerService = {};
-let serviceTotalBrowsers = {};
+const data = JSON.parse(dataAsJSON);
 
-const getPercentagePerBrowser = (views, service) =>
-  ((views / serviceTotalBrowsers[service]) * 100).toFixed(3);
+// pass over data removing all invalid enteries
+const validData = removeInvalidValues(data);
 
-const countAllViews = (service, browserViews, countArray) => {
-  // if the array doesn't have the service name as a key
-  if (!countArray.hasOwnProperty(service)) {
-    // add a new array key with the value
-    countArray[service] = parseInt(browserViews);
-  } else {
-    // if the service is already a key in the array just add the value
-    countArray[service] += parseInt(browserViews);
-  }
-};
+// get the total per each service
+const totalBrowsersPerService = getServiceTotalBrowsers(validData);
 
-/*
-  This method allows an empty object to be populated with values that have matching keys.
-  It does this by checking if the key exists and either creating it or cumulatively adding to the key's value
-*/
-const addToDynamicallyKeyedObject = (firstKey, secondKey, value, object) => {
-  // if the array doesn't have the service name as a key
-  if (!object.hasOwnProperty(firstKey)) {
-    object[firstKey] = {};
-  }
+// remove enteries not in threshold
+const dataWithSupportThreshold = getDataWithinThreshold(
+  validData,
+  totalBrowsersPerService
+);
 
-  object[firstKey][secondKey] = value;
-};
+const getDataWithBrowsersAndVersionArray = seperateBrowserNameAndVersion(
+  dataWithSupportThreshold
+);
 
-const generatorBrowserPercentagesObject = data => {
-  data.forEach(item => {
-    if (isValidBBCSiteValue(item["bbc_site"])) {
-      countAllViews(item["bbc_site"], item["Browsers"], serviceTotalBrowsers);
-
-      addToDynamicallyKeyedObject(
-        item["bbc_site"],
-        item["Web browser"],
-        item["Browsers"],
-        dataKeyedPerService
-      );
-    }
-  });
-};
-
-const splitVersionFromBrowser = browserNameWithVersion => {
-  // get the version numbers from the current browserDataWithVersion entry
-  const versionRegex = / ([0-9]+)(\.x)/;
-
-  // return the full value for things like `Nokia 6280` and `MAUI WAP Browser`
-  let version = browserNameWithVersion;
-  let browserName = browserNameWithVersion;
-
-  const matches = browserNameWithVersion.match(versionRegex);
-
-  if (matches) {
-    browserName = browserNameWithVersion.replace(matches[0], "");
-    version = parseInt(matches[1]);
-  }
-
-  return { version, browserName };
-};
-
-const checkBrowserStatsMeetSupportCriteria = (
-  browserDataWithVersion,
-  service
-) => {
-  const percentageBrowsers = getPercentagePerBrowser(
-    browserDataWithVersion,
-    service
-  );
-
-  // if the percentage value is above our support threshold
-  if (percentageBrowsers > 0.05) {
-    return browserDataWithVersion;
-  }
-};
-
-const updateBrowserKeysAndAddLowestVersionValues = (
-  service,
-  browserNameWithVersion,
-  browserName,
-  version
-) => {
-  // if the browser key doesn't yet exist create it
-  if (!dataKeyedPerService[service].hasOwnProperty(browserName)) {
-    dataKeyedPerService[service][browserName] = version;
-  }
-  // set the value to the lowest version
-  if (version < dataKeyedPerService[service][browserName]) {
-    dataKeyedPerService[service][browserName] = version;
-  }
-
-  delete dataKeyedPerService[service][browserNameWithVersion];
-
-  if (browserNameWithVersion === "Unknown") {
-    delete dataKeyedPerService[service][browserNameWithVersion];
-  }
-};
-
-const handleDataPerService = service => {
-  const browserKeys = Object.keys(dataKeyedPerService[service]);
-
-  const browsers = browserKeys.map(browserNameWithVersion => {
-    const browserDataWithVersion =
-      dataKeyedPerService[service][browserNameWithVersion];
-    if (checkBrowserStatsMeetSupportCriteria(browserDataWithVersion, service)) {
-      const { browserName, version } = splitVersionFromBrowser(
-        browserNameWithVersion
-      );
-
-      updateBrowserKeysAndAddLowestVersionValues(
-        service,
-        browserNameWithVersion,
-        browserName,
-        version
-      );
-    }
-  });
-
-  browsers.forEach(browser => {
-    const lowestVersion = dataKeyedPerService[service][browser];
-    console.log(
-      `| ${service}                     | ${browser}           | ${lowestVersion}    |`
-    );
-  });
-};
-
-// const consoleLogSupportedBrowserVersions = () => {
-//   const services = Object.keys(dataKeyedPerService);
-
-//   console.log(
-//     "| Service                     | Browser           | Lowest version    |"
-//   );
-//   console.log(
-//     "| --------------------------- | ----------------- | ---------- |"
-//   );
-
-//   services.forEach(service => {
-//     const browsers = Object.keys(dataKeyedPerService[service]);
-
-//     browsers.forEach(browser => {
-//       const lowestVersion = dataKeyedPerService[service][browser];
-//       console.log(
-//         `| ${service}                     | ${browser}           | ${lowestVersion}    |`
-//       );
-//     });
-//   });
-// };
-
-const getManipulatedData = () => {
-  const services = Object.keys(dataKeyedPerService);
-
-  services.forEach(service => {
-    handleDataPerService(service);
-  });
-};
-
-const getLowestBrowserVersionPercentages = data => {
-  generatorBrowserPercentagesObject(data);
+const getBrowserWithLowestSupportVersions = () => {
+  const data = getDataWithBrowsersAndVersionArray;
 
   console.log(
     "| Service                     | Browser           | Lowest version    |"
@@ -186,7 +48,20 @@ const getLowestBrowserVersionPercentages = data => {
     "| --------------------------- | ----------------- | ---------- |"
   );
 
-  getManipulatedData();
+  const services = Object.keys(data);
+
+  services.forEach(service => {
+    const browsers = Object.keys(data[service]);
+
+    browsers.forEach(browser => {
+      const browserVersions = data[service][browser];
+      const lowestVersion = Math.min.apply(null, browserVersions);
+
+      console.log(
+        `| ${service}        | ${browser}           | ${lowestVersion}    |`
+      );
+    });
+  });
 };
 
-getLowestBrowserVersionPercentages(JSON.parse(data));
+getBrowserWithLowestSupportVersions();
